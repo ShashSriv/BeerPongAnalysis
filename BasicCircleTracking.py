32909cffcabbb72c817c5ec2a25e3f329bb32a5b
ShashEatsBeans <shashwat.avin@gmail.com>
2024-09-25 17:30:51 -0400

    Finished basic circle tracking implementation, will need to experiment with other detection methods for my project still

--- BasicCircleTracking.py
+++ BasicCircleTracking.py
@@ -1,10 +1,14 @@
 import cv2 as cv
 import numpy as np
 
+
+
+##Hough Circle Detection Distance Function
+dist = lambda x1, y1, x2, y2: (x1 - x2)**2 + (y1 - y2)**2 ##We will use the swuare of the distance to compare
+
 ## Opening the webcam on laptop
 videoCapture = cv.VideoCapture(0, cv.CAP_DSHOW)
 prevCircle = None
-dist = lambda x1, y1, x2, y2: (x1 - x2)**2 + (y1 - y2)**2
 
 ##Check if the camera was opened successfully
 if videoCapture.isOpened() is False:
@@ -19,13 +23,13 @@ while True:
         print("Error: Frame was not captures succesfully.")
         break
 
-    ##Applying grayscale and blur to the frame
+    ##Applying grayscale and blur to the frame for better circle detection from HoughCirles
     grayFrame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     blurFrame = cv.GaussianBlur(grayFrame, (17, 17), 0)
 
     ##Using HoughCircles to detect circle in frame, set min distance at 100 due to wanting to only detect one circle
     circles = cv.HoughCircles(blurFrame, cv.HOUGH_GRADIENT, 1.1, 100, 
-                              param1 = 100, param2 = 60, minRadius = 10, maxRadius = 400)
+                              param1 = 100, param2 = 55, minRadius = 5, maxRadius = 400)
     
     #Condition that draws circle around the detected circle, and also chooses the best circle to track
     if circles is not None:
